5a07d712e70dc97bdcb65f9050f9a5377c2add55
ZuhayrK00 <zuhayrk00@gmail.com>
2023-06-09 14:27:12 +0100

    added select all function to country repo

--- repository/country_repository.py
+++ repository/country_repository.py
@@ -10,3 +10,14 @@ def save(country):
     id = results[0]['id']
     country.id = id
     return country
+
+def select_all():
+    countries = []
+
+    sql = "SELECT * FROM countries"
+    results = run_sql(sql)
+
+    for row in results:
+        country = Country(row['country_name'], row['continent'], row['id'])
+        countries.append(country)
+    return countries
