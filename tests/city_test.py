4006a4d44795f04010e05920392b3c495218cb58
ZuhayrK00 <zuhayrk00@gmail.com>
2023-06-11 00:34:56 +0100

    added testing for city

---
+++ tests/city_test.py
@@ -0,0 +1,15 @@
+import unittest
+from models.city import City
+from models.country import Country
+
+class TestCity(unittest.TestCase):
+
+    def setUp(self):
+        self.city1 = City("Edinburgh", "Scotland")
+        self.city2 = City("Barcalona", "Spain")
+
+    def test_city_has_country(self):
+        self.assertEqual("Edinburgh", self.city1.country)
+
+    def test_city_has_name(self):
+        self.assertEqual("Barcalona", self.city2.name)
