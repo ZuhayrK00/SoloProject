add8ef3866411e2cbd45b1f398364dff0ff8d4d9
ZuhayrK00 <zuhayrk00@gmail.com>
2023-06-11 00:45:17 +0100

    added select by id to country repo

--- repositories/country_repository.py
+++ repositories/country_repository.py
@@ -90,3 +90,19 @@ def select_all():
         )
         countries.append(country)
     return countries
+
+def select(id):
+    country = None
+    sql = "SELECT * FROM countries WHERE id = %s"
+    values = [id]
+    result = run_sql(sql, values)[0]
+
+    if result is not None:
+        country = Country(
+            result["name"],
+            result["continent"],
+            result["language"],
+            result["visited"],
+            result["id"],
+        )
+    return country
