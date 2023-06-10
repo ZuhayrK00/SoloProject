195b7bbe42c69160b0c4be7fda8799351a5bf092
ZuhayrK00 <zuhayrk00@gmail.com>
2023-06-11 00:42:52 +0100

    added save function to country repo

--- repositories/country_repository.py
+++ repositories/country_repository.py
@@ -65,3 +65,15 @@ def update(country):
         country.id,
     ]
     run_sql(sql, values)
+from db.run_sql import run_sql
+
+from models.country import Country
+from models.city import City
+
+def save(country):
+    sql = "INSERT INTO countries (name, continent, language, visited) VALUES (%s, %s, %s, %s) RETURNING *"
+    values = [country.name, country.continent, country.language, country.visited]
+    results = run_sql(sql, values)
+    id = results[0]["id"]
+    country.id = id
+    return country
