d9901b5b875235df473b06a003c3d4b6082dab66
ZuhayrK00 <zuhayrk00@gmail.com>
2023-06-12 18:58:17 +0100

    added bucketlist controller

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ from flask import Flask, render_template
 
 from controllers.cities_controller import cities_blueprint
 from controllers.countries_controller import countries_blueprint
+from controllers.bucketlist_controller import bucketlist_blueprint
 
 app = Flask(__name__)
 
 app.register_blueprint(cities_blueprint)
 app.register_blueprint(countries_blueprint)
+app.register_blueprint(bucketlist_blueprint)
 
 
 @app.route("/")
---
+++ controllers/bucketlist_controller.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request, redirect
+from flask import Blueprint
+import repositories.city_repository as city_repository
+import repositories.country_repository as country_repository
+
+bucketlist_blueprint = Blueprint("bucketlist", __name__)
+
+
+@bucketlist_blueprint.route("/bucketlist")
+def bucketlist():
+
+    bucketlist_cities = [city for city in city_repository.select_all() if city.visited is False]
+
+    countries = country_repository.select_all()
+    bucketlist_countries = []
+    for country in countries:
+        if country.visited is False:
+            bucketlist_countries.append(country)
+
+    # import pdb; pdb.set_trace()
+    return render_template("/bucketlist.html", all_cities=bucketlist_cities, all_countries = bucketlist_countries)
